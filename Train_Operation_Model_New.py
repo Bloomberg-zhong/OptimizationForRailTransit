945242367d2fd6dedf6200dbf037b7a41a394699
zhonon <z136303452@hotmail.com>
2019-04-11 15:11:24 +0800

    新版本调整

--- Train_Operation_Model_New.py
+++ Train_Operation_Model_New.py
@@ -24,7 +24,8 @@ class modle():
                  x: 5,  # 5< = x <=24
                  f1: 5,
                  f2: 5,
-                 G: None
+                 G: None,
+                 K: None
                  ):
         # Q 为列车流量
         P = (pd.read_excel(data_file_path, sheet_name=0, index_col=0))
@@ -47,45 +48,53 @@ class modle():
         Tij = pd.read_excel(data_file_path, sheet_name=3, index_col=0)
         self.Tij = Tij .iloc[:, 1:].fillna(0)
 
-        CT_I_J = pd.read_excel(data_file_path, sheet_name=5, index_col=0)
-        self.CT_I_J = CT_I_J.iloc[:, 1:]
         self.f1 = f1  # 主线交路的开行频率 （对数）
         self.h1 = 1 / f1  # 主线交路的发车间隔（时间）
         self.f2 = f2  # 支线交路的开行频率 （对数）
         self.h2 = 1 / f2  # 支线交路的发车间隔（时间）
+        self.C_Z = 800  # 列车固定人员数
+        self.T0 = 15
 
-        if 0.05 <= G and G <= 0.15:
-            self.G = G  # 大小交路发车间隔
-        else:
-            return u'G的值需要在约束条件中。。。'
         self.x = x
-
-    def Constraint(self, K):
         """
         K  = 满载率αk
         """
-        C_Z = 800  # 列车固定人员数
-        if K < self.x:
-            FH = self.f1
-
-        elif 8 <= K and K <= 12:
-            FH = self.f1
-
-        elif 12 < K and K <= 18:
-            FH = self.f2
+        if 0.05 <= G and G <= 0.15:
+            self.G = G  # 大小交路发车间隔
+            print("G的范围合理！G:" + str(G))
         else:
-            # 当K>X的时候，
-            FH = (self.f1 + self.f2) / 2
+            raise Exception('G的值需要在约束条件中0.05<=G<=0.15')
 
         if self.x == 0:
-            self.f1 + self.f2 <= 24
-        if self.x == 7:
-            self.f1 > 5
-            self.f2 > 5
-            self.f1 <= 24
-            self.f2 <= 24
+            if self.f1 + self.f2 <= 24:
+                raise Exception(u'错误:当x=0 '
+                                u'\tf1 + f2 <= 24')
 
-    # 乘客候车时间CW
+        if self.x == 7:
+            if self.f1 > 5:
+                raise Exception(u'错误:当x=7'
+                                u'\tf1>5')
+            if self.f2 > 5:
+                raise Exception(u'错误:当x=7'
+                                u'\tf2>5')
+            if self.f1 <= 24:
+                raise Exception(u'错误:当x=7'
+                                u'\tf1<=24')
+            if self.f2 <= 24:
+                raise Exception(u'错误:当x=7'
+                                u'\tf2<=24')
+
+        if K < self.x:
+            self.FH = self.f1
+
+        elif 8 <= K and K <= 12:
+            self.FH = self.f1
+
+        elif 12 < K and K <= 18:
+            self.FH = self.f2
+        else:
+            # 当K>X的时候，
+            self.FH = (self.f1 + self.f2) / 2
 
     def Passenger_Waiting_Time(self,
                                ):
@@ -103,12 +112,11 @@ class modle():
         CW3 = (self.h1 / 2) * np.sum(np.array(self.P.iloc[8:12, 8:12]))
         CW4 = (self.h2 / 2) * np.sum(np.array(self.P.iloc[12:, 12:]))
         CW = CW1 + CW2 + CW3 + CW4
-
+        print('乘客候车时间CW')
+        print('\tCW:' + str(CW))
         return CW
 
-    def Passenger_Travel_Time(self,
-                              K: None
-                              ):
+    def Passenger_Travel_Time(self,):
         """
         K: 非换成节点
         X: 换成节点
@@ -118,7 +126,7 @@ class modle():
                 CV = CVR + ST
         """
         # 乘客乘车时间由途中纯运行时间CVR
-        CVR = np.sum(np.array(self.P) * self.Tij)
+        CVR = np.sum(np.array(self.P) * np.array(self.Tij))
 
         D = 1.53
         V = 4  # 列车编组量数
@@ -126,32 +134,22 @@ class modle():
         ST_0 = 17  # 开门时间
 
         # ST列车在车站停留时间
-        # 当K<X的时候，
-        if K < self.x:
-            FH = self.f1
-
-        elif 8 <= K and K <= 12:
-            FH = self.f1
-
-        elif 12 < K and K <= 18:
-            FH = self.f2
-        else:
-            # 当K>X的时候，
-            FH = (self.f1 + self.f2) / 2
-
         # 交路1的每列列车在车站K的停站时间——1
         # ST_0列车在车站开关门时间
 
-        ST_H_K = pd.DataFrame((np.array(self.ak) * D) / (V * N_V * FH) + ST_0)
-
-        ST_K = ((np.array(self.Train_Values_of_Float)[:, 2:3])) * ST_H_K
+        ST_H_K = pd.DataFrame((np.array(self.ak) * D) /
+                              (V * N_V * self.FH) + ST_0)
+        ST_K = ((np.array(self.Train_Values_of_Float))) * ST_H_K
 
         ST_K = np.sum(ST_K)
         # ST列车在车站停留时间
         ST = sum(ST_K)
         # 乘客候车时间CW, 乘客在车时间CV，乘客换乘时间CT
         CV = CVR + ST
-        print("CV:" + str(CV))
+        print('乘客乘车时间CV')
+        print("\tCVR:" + str(CVR))
+        print("\tST:" + str(ST))
+        print("\tCV:" + str(CV))
         return CV, CVR
 
     def Passenger_Transfer_Time(self):
@@ -160,10 +158,18 @@ class modle():
             乘客在站台的换乘时间CT
         """
         # 乘客在站台的换乘时间CT
-        CT = np.sum(np.array(self.CT_I_J) * np.array(self.P))
-        print(str(CT))
+        Time = (3600 / (2 * self.f2)) + self.T0
+        Train_time_cros = pd.DataFrame(np.zeros(shape=(18, 18)))
+        Train_time_cros.iloc[:self.x,
+                             11:] = Train_time_cros.iloc[:self.x,
+                                                         11:].replace(0,
+                                                                      Time)
+        CT = np.sum(np.array(Train_time_cros) * np.array(self.P))
+        print('乘客在站台的换乘时间CT')
+        print('CT:' + str(CT))
+        return CT
 
-    def The_Train_Goes_Miles(self, L_2):
+    def The_Train_Goes_Miles(self,):
         L_1 = 48657
         if 0 <= self.x and self.x <= 7:
             pass
@@ -172,3 +178,20 @@ class modle():
 
         L_2 = self.The_Train_Miles.iloc[self.x:7, 3:4].sum().values[0] \
             + self.The_Train_Miles.iloc[11:17, 3:4].sum().values[0]
+        print('列车长度L2')
+        print("L_2:" + str(L_2))
+        return L_1, L_2
+
+
+if __name__ == '__main__':
+    M = modle(
+        x=6,
+        f1=6,
+        f2=6,
+        G=0.05,
+        K=5
+    )
+    # M.Passenger_Waiting_Time()
+#     M.Passenger_Travel_Time()
+#     M.Passenger_Transfer_Time()
+    M.The_Train_Goes_Miles()
